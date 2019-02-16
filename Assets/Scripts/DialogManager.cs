a0b27cb75a60f53cef0243466268d2785fc850fc
Arvin DSouza <arvin.dsouza4@gmail.com>
2019-02-16 19:10:47 +0300

    Section 6

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text dialogText, nameText;
+    public GameObject dialogBox, nameBox;
+    public string[] dialogLines;
+    public int curLine;
+    private bool firstStart = true;
+
+    public static DialogManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //dialogText.text = dialogLines[curLine];
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogBox.activeInHierarchy)
+        {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                if (!firstStart)
+                {
+                    curLine++;
+
+                    if (curLine >= dialogLines.Length)
+                    {
+                        dialogBox.SetActive(false);
+                        PlayerController.instance.canMove = true;
+                        firstStart = true;
+                    }
+                    else
+                    {
+                        CheckIfName();
+                        dialogText.text = dialogLines[curLine];
+                    }
+                }
+                else
+                {
+                    firstStart = false;
+                }
+            }
+        }
+    }
+
+    public void ShowDialog(string[] theLines, bool isPerson)
+    {
+        dialogLines = theLines;
+        curLine = 0;
+        CheckIfName();
+        PlayerController.instance.canMove = false;
+        dialogText.text = dialogLines[curLine];
+        dialogBox.SetActive(true);
+
+        nameBox.SetActive(isPerson);
+    }
+
+    public void CheckIfName()
+    {
+        if (dialogLines[curLine].StartsWith("n-"))
+        {
+            nameText.text = dialogLines[curLine].Replace("n-","");
+            curLine++;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D theRB;
     public Animator myAnim;
     public float moveSpeed;
+    public bool canMove = true;
 
     public static PlayerController instance;
 
@@ -28,15 +29,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        if (canMove)
+        {
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        } else
+        {
+            theRB.velocity = Vector2.zero;
+        }
         
         myAnim.SetFloat("moveX", theRB.velocity.x);
         myAnim.SetFloat("moveY", theRB.velocity.y);
 
         if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
         {
-            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            if (canMove)
+            {
+                myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+                myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            }
         }
 
         //keep player inside bounds
