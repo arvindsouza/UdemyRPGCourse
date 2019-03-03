09f6cabd063adff249be6fa85c9292647c3c60c3
Arvin DSouza <arvin.dsouza4@gmail.com>
2019-03-03 10:33:55 +0300

    Finished section 9

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [Header("Item type")]
+    public bool isItem;
+    public bool isWeapon, isArmor;
+
+    [Header("Item details")]
+    public int value;
+    public Sprite itemSprite;
+    public string itemName, desc;
+
+    [Header("Item details")]
+    public int amtToChange;
+    public bool affectHP, affectMP, affectStr;
+
+    [Header("Weapon/Armor details")]
+    public int wpnStr;
+    public int armStr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Use(int charToUseOn)
+    {
+        CharStats selectedChar = GameManager.instance.playerStats[charToUseOn];
+
+        if (isItem)
+        {
+            if (affectHP)
+            {
+                selectedChar.curHP += amtToChange;
+                if(selectedChar.curHP > selectedChar.maxHP)
+                {
+                    selectedChar.curHP = selectedChar.maxHP;
+                }
+            }
+
+            if (affectMP)
+            {
+                selectedChar.curHP += amtToChange;
+                if(selectedChar.curMP > selectedChar.maxMP)
+                {
+                    selectedChar.curMP = selectedChar.maxMP;
+                }
+            }
+
+            if (affectStr)
+            {
+                selectedChar.strength += amtToChange;
+            }
+        }
+
+        if (isWeapon)
+        {
+            if(selectedChar.equippedWpn != "")
+            {
+                GameManager.instance.AddItem(selectedChar.equippedWpn);
+            }
+            selectedChar.equippedWpn = itemName;
+            selectedChar.weaponPower = wpnStr;
+        }
+
+        if (isArmor)
+        {
+            if (selectedChar.equippedArmor != "")
+            {
+                GameManager.instance.AddItem(selectedChar.equippedArmor);
+            }
+            selectedChar.equippedArmor = itemName;
+            selectedChar.armorPower = armStr;
+        }
+
+        GameManager.instance.RemoveItem(itemName);
+    }
+}
