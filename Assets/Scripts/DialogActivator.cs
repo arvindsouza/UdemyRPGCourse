7acfa3bcf960697143a1bf17ff081bc671c69aad
Arvin DSouza <arvin.dsouza4@gmail.com>
2019-03-04 20:01:06 +0300

    Completed section 11

--- Assets/Scripts/DialogActivator.cs
+++ Assets/Scripts/DialogActivator.cs
@@ -7,7 +7,9 @@ public class DialogActivator : MonoBehaviour
     public string[] lines;
 
     private bool canActivate;
-    public bool isPerson = true;
+    public bool isPerson = true, shouldActivateQuest, markComplete;
+    public string questToMark;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,7 @@ public class DialogActivator : MonoBehaviour
     {
         if(canActivate && Input.GetButtonDown("Fire1") && !DialogManager.instance.dialogBox.activeInHierarchy){
             DialogManager.instance.ShowDialog(lines, isPerson);
+            DialogManager.instance.ShouldActivateQuestAtEnd(questToMark, markComplete);
         }
     }
 
