c43d7e9a06e43476f59df50dfc3e58deca2ab610
arvindsouza <30589934+arvindsouza@users.noreply.github.com>
2019-03-06 13:36:46 +0300

    Finished section 12 (#9)

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -212,4 +212,10 @@ public class GameMenu : MonoBehaviour
         activeItem.Use(selectChar);
         CloseItemCharChoice();
     }
+
+    public void SaveGame()
+    {
+        GameManager.instance.SaveData();
+        QuestManager.instance.SaveQuestData();
+    }
 }
